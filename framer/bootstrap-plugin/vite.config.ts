a96edd9356da56447257fbdd364de70e375f5555
Winner <nnamuahn@gmail.com>
2026-09-11 19:05:37 +0100

    Fix Framer local development connection

--- framer/bootstrap-plugin/vite.config.ts
+++ framer/bootstrap-plugin/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import mkcert from "vite-plugin-mkcert"
 import framer from "vite-plugin-framer"
 
 export default defineConfig({
-  plugins: [react(), framer()],
+  plugins: [react(), mkcert(), framer()],
   build: {
     outDir: "dist",
   },
